25064b7c01cd8e0b6037bfaa6d669102d47813ff
shivam904455 <shivamrajpoot904455@gmail.com>
2023-04-09 20:13:02 +0530

    Ifelse condition

---
+++ IfElse-condition/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	if true {
+
+	} else {
+
+	}
+	if 5 < 10 {
+		fmt.Println("5<10 : yes its true ")
+	} else {
+		fmt.Println("5>10: yes its wrong")
+	}
+}
