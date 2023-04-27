00b71029867f9f7cbfe32efc4234fa49d0607b98
shivam904455 <shivamrajpoot904455@gmail.com>
2023-04-27 16:51:00 +0530

    Datastructure

--- IfElse-condition/main.go
+++ IfElse-condition/main.go
@@ -1,16 +1,8 @@
 package main
 
+
 import "fmt"
 
 func main() {
-	if true {
-
-	} else {
-
-	}
-	if 5 < 10 {
-		fmt.Println("5<10 : yes its true ")
-	} else {
-		fmt.Println("5>10: yes its wrong")
-	}
+	("hello world")
 }
