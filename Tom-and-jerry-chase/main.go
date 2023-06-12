1ba1e83cc0dda5c78566eceb9b2afb503a4a3bc6
shivam904455 <shivamrajpoot904455@gmail.com>
2023-06-12 15:34:05 +0530

    Tom and jerry

---
+++ Tom-and-jerry-chase/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    var T int
+    fmt.Scan(&T)
+
+    for i := 0; i < T; i++ {
+        var X, Y int
+        fmt.Scan(&X, &Y)
+
+        if X <= Y {
+            fmt.Println("YES")
+        } else {
+            fmt.Println("NO")
+        }
+    }
+}
+ 
